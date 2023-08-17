2e47f5f0c2cd6e2ac6b8386f4da10a145ee84ae7
Vasilije Tomovic <vasilije@upment.com>
2023-08-17 21:51:54 +0200

    Filter example, display just people with chemist profession.

--- src/RenderingList.js
+++ src/RenderingList.js
@@ -1,16 +1,40 @@
-const people = [
-    'Creola Katherine Johnson: mathematician',
-    'Mario José Molina-Pasquel Henríquez: chemist',
-    'Mohammad Abdus Salam: physicist',
-    'Percy Lavon Julian: chemist',
-    'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+// const people = [
+//     'Creola Katherine Johnson: mathematician',
+//     'Mario José Molina-Pasquel Henríquez: chemist',
+//     'Mohammad Abdus Salam: physicist',
+//     'Percy Lavon Julian: chemist',
+//     'Subrahmanyan Chandrasekhar: astrophysicist'
+// ];
+//
+//
+//
+//
+// export default function RenderingList() {
+//     const listItems = people.map(person => <li>{person}</li>);
+//
+//     return <ul>{listItems}</ul>;
+// }
 
 
-
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
 
 export default function RenderingList() {
-    const listItems = people.map(person => <li>{person}</li>);
-
+    const chemists = people.filter(person =>
+        person.profession === 'chemist'
+    );
+    const listItems = chemists.map(person =>
+        <li>
+            <img
+                src={getImageUrl(person)}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}:</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+            </p>
+        </li>
+    );
     return <ul>{listItems}</ul>;
 }
