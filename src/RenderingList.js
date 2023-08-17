10f2c9dd008230466dcbc5905eecc981c7681ed2
Vasilije Tomovic <vasilije@upment.com>
2023-08-17 21:59:46 +0200

    Error log for keys bug fixed.

--- src/RenderingList.js
+++ src/RenderingList.js
@@ -18,19 +18,20 @@
 
 import { people } from './data.js';
 import { getImageUrl } from './utils.js';
+import './list.css';
 
 export default function RenderingList() {
-    const chemists = people.filter(person =>
-        person.profession === 'chemist'
-    );
-    const listItems = chemists.map(person =>
-        <li>
+    // const chemists = people.filter(person =>
+    //     person.profession === 'chemist'
+    // );
+    const listItems = people.map(person =>
+        <li key={person.id}>
             <img
                 src={getImageUrl(person)}
                 alt={person.name}
             />
             <p>
-                <b>{person.name}:</b>
+                <b>{person.name}</b>
                 {' ' + person.profession + ' '}
                 known for {person.accomplishment}
             </p>
